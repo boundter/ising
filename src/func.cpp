9af088aa8be8a8767c6e35d90d7a7a12ead93d02
Erik Teichmann <erik-teichmann@gmx.de>
2017-01-24 09:58:08 +0100

    Add function to measure statistics

--- src/func.cpp
+++ src/func.cpp
@@ -3,3 +3,12 @@
 void WriteHeader(Ising ising, FILE * datafile) {
   fprintf(datafile, "# L= %d , T= %.9f \n", ising.L(), ising.T());
 }
+
+
+void UpdateStat(const double& value, double& mean, double& M2, double& n) {
+  n += 1;
+  double delta = value - mean;
+  mean += delta/n;
+  double delta_2 = value - mean;
+  M2 += delta*delta_2;
+}
--- src/func.h
+++ src/func.h
@@ -4,6 +4,12 @@
 #include <cstdio>
 #include "ising.h"
 
+// Write a header to the datafile containing L and T.
 void WriteHeader(Ising ising, FILE * datafile);
+
+
+// Calculate the moving average and variance using the Welford-algorithm.
+// Var = M2/n
+void UpdateStat(const double& value, double& mean, double& M2, double& n);
 
 #endif
