aaf273ec87fbd3fe0797c6f63c2f7935c71ed780
Erik Teichmann <erik-teichmann@gmx.de>
2017-01-26 10:58:40 +0100

    Add simulation for <m>(T)

--- makefile
+++ makefile
@@ -1,8 +1,16 @@
 SOURCES = src/main.cpp src/ising.cpp src/func.cpp
 EXEC = bin/ising.o
+LIBS = src/ising.cpp src/func.cpp
+T_CRIT_SOURCES = src/main_T_crit.cpp
+T_CRIT_EXEC = bin/T_crit.o
 
 all: $(EXEC)
+t_crit: $(T_CRIT_EXEC)
 
 $(EXEC): $(SOURCES)
-	g++ -o $(EXEC) $(SOURCES) -O3;
+	g++ -o $(EXEC) $(SOURCES) $(LIBS) -O3;
 	$(EXEC)
+
+$(T_CRIT_EXEC): $(T_CRIT_SOURCES)
+	g++ -o $(T_CRIT_EXEC) $(T_CRIT_SOURCES) $(LIBS) -O3;
+	$(T_CRIT_EXEC)
---
+++ src/main_T_crit.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include "ising.h"
+#include "func.h"
+
+using namespace std;
+
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0])) // length of an array
+
+int L[3] = {20, 50, 80};
+double T_min = 0.5, T_max = 4.0, n_T = 30, T = T_min;
+double n_sweeps = 100000;
+string folder = "result/data/";
+
+int main() {
+  double T_step = (T_max - T_min)/(n_T - 1);
+  string filename = folder + "T_crit.dat";
+  FILE * datafile = fopen(filename.c_str(), "w");
+
+  for (int l = 0; l < NELEMS(L); ++l) {
+    T = T_min;
+    Ising ising(L[l], T);
+
+    for (int t = 0; t < n_T; ++t) {
+      double mean = 0, M2 = 0, n = 0;
+      ising.SetT(T);
+      T += T_step;
+      for (int i = 0; i < n_sweeps; ++i) {
+        for (int j = 0; j < ising.L()*ising.L(); ++j) {
+          ising.Flip();
+        }
+        UpdateStat(ising.M(), mean, M2, n);
+      }
+      fprintf(datafile, "%d %d %.9f %.9f %.9f\n", l, ising.L(), ising.T(), mean,
+                                                M2/n);
+      fflush(datafile);
+    }
+  }
+  fclose(datafile);
+}
