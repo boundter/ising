e221459721a10cc6a115552acca8d894198e3d87
Erik Teichmann <erik-teichmann@gmx.de>
2017-01-27 15:46:40 +0100

    Sync commit

--- src/main_T_crit.cpp
+++ src/main_T_crit.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 #define NELEMS(x)  (sizeof(x) / sizeof((x)[0])) // length of an array
 
-int L[3] = {20, 50, 80};
-double T_min = 0.5, T_max = 4.0, n_T = 50, T = T_min;
+int L[5] = {20, 40, 60, 80, 100};
+double T_min = 0.5, T_max = 4.0, n_T = 60, T = T_min;
 double n_trans = 60000;
 double n_sweeps = 100000;
 string folder = "result/data/";
