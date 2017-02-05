891a0300c86b3521f6c7f7bbc3ef7d8b9d75d110
Erik Teichmann <erik-teichmann@gmx.de>
2017-02-05 11:16:19 +0100

    Sync commit

--- src/main_T_crit.cpp
+++ src/main_T_crit.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <string>
 #include <vector>
+#include <cmath>
+
 #include "ising.h"
 #include "func.h"
 
@@ -9,10 +11,20 @@ using namespace std;
 #define NELEMS(x)  (sizeof(x) / sizeof((x)[0])) // length of an array
 
 int L[5] = {20, 40, 60, 80, 100};
-double T_min = 0.5, T_max = 4.0, n_T = 60, T = T_min;
+double T_min = 0.5, T_max = 4.0, n_T = 50, T = T_min;
 double n_trans = 20000;
-double n_sweeps = 100000;
+double n_sweeps = 10000;
+int n_bootstrap = 1000;
 string folder = "result/data/";
+
+
+double Average(const vector<double>& data, void* params) {
+  double mean = 0;
+  for (int i = 0; i < data.size(); ++i) {
+    mean += data[i];
+  }
+  return mean/double(data.size());
+}
 
 int main() {
   double T_step = (T_max - T_min)/(n_T - 1);
@@ -26,19 +38,24 @@ int main() {
 
 
     for (int t = 0; t < n_T; ++t) {
-      double mean = 0, M2 = 0, n = 0;
       ising.SetT(T);
       T += T_step;
+      vector<double> M;
+      for (int i = 0; i < n_trans; ++i) {
+        for (int j = 0; j < ising.L()*ising.L(); ++j) {
+          ising.Flip();
+        }
+      }
       for (int i = 0; i < n_sweeps; ++i) {
         for (int j = 0; j < ising.L()*ising.L(); ++j) {
           ising.Flip();
         }
-        if (i >= n_trans) {
-          UpdateStat(ising.M(), mean, M2, n);
-        }
+        M.push_back(fabs(ising.M()/(double(ising.L())*double(ising.L()))));
       }
+      double mean = Average(M, NULL);
+      double stdev = sqrt(Bootstrap(n_bootstrap, M, Average, NULL));
       fprintf(datafile, "%d %d %.9f %.9f %.9f\n", l, ising.L(), ising.T(), mean,
-                                                M2/n);
+                                                stdev);
       fflush(datafile);
     }
   }
--- src/main_finite_size.cpp
+++ src/main_finite_size.cpp
@@ -9,10 +9,33 @@ using namespace std;
 #define NELEMS(x)  (sizeof(x) / sizeof((x)[0])) // length of an array
 
 int L[5] = {20, 40, 60, 80, 100};
-double n_T = 50, T_c = 2.269;
-double n_trans = 60000;
-double n_sweeps = 100000;
+double n_T = 50, T, T_c = 2.269;
+double n_trans = 50000;
+double n_sweeps = 50000;
+int n_bootstrap = 1000;
 string folder = "result/data/";
+
+
+double Variance(const vector<double>& data) {
+  double mean = 0, var = 0;
+  for (int i = 0; i < data.size(); ++i) {
+    mean += data[i];
+  }
+  mean /= double(data.size());
+  for (int i = 0; i < data.size(); ++i) {
+    var += (data[i] - mean)*(data[i] - mean);
+  }
+  return var/double(data.size());
+}
+
+
+double Susceptibility(const vector<double>& data, void* params) {
+  // cons[0] = T, cons[1] = L
+  double* cons = (double *) params;
+  double var = Variance(data);
+  return cons[1]*cons[1]/cons[0]*var;
+}
+
 
 int main() {
   string filename = folder + "finite.dat";
@@ -22,24 +45,32 @@ int main() {
     double T_max = T_c + 10*T_c/L[l];
     double T_min = T_c - 10*T_c/L[l];
     double T_step = (T_max - T_min)/(n_T - 1);
-
-    double T = T_min;
+    T = T_min;
     Ising ising(L[l], T);
+    ising.SetUniformState();
+
 
     for (int t = 0; t < n_T; ++t) {
-      double mean = 0, M2 = 0, n = 0;
       ising.SetT(T);
       T += T_step;
+      vector<double> M;
+      for (int i = 0; i < n_trans; ++i) {
+        for (int j = 0; j < ising.L()*ising.L(); ++j) {
+          ising.Flip();
+        }
+      }
       for (int i = 0; i < n_sweeps; ++i) {
         for (int j = 0; j < ising.L()*ising.L(); ++j) {
           ising.Flip();
         }
-        if (i >= n_trans) {
-          UpdateStat(ising.M(), mean, M2, n);
-        }
+        M.push_back(fabs(ising.M()/(double(ising.L())*double(ising.L()))));
       }
-      fprintf(datafile, "%d %d %.9f %.9f %.9f\n", l, ising.L(), ising.T(), mean,
-                                                M2/n);
+      double cons[] = {ising.T(), double(ising.L())};
+      void* params = &cons;
+      double susc = Susceptibility(M, params);
+      double stdev = sqrt(Bootstrap(n_bootstrap, M, Susceptibility,params));
+      fprintf(datafile, "%d %d %.9f %.9f %.9f\n", l, ising.L(), ising.T(), susc,
+                                                stdev);
       fflush(datafile);
     }
   }
