70d75df1f7072e86fde4f34a5225353e2d502739
Erik Teichmann <erik-teichmann@gmx.de>
2017-02-05 11:15:18 +0100

    Add simulation for susceptibility

--- makefile
+++ makefile
@@ -1,17 +1,20 @@
 LIBS = src/ising.cpp src/func.cpp
 T_CRIT_SOURCES = src/main_T_crit.cpp
 T_CRIT_EXEC = bin/T_crit.o
-FINITE_SOURCES = src/main_finite.cpp
+FINITE_SOURCES = src/main_finite_size.cpp
 FINITE_EXEC = bin/finite.o
 EVO_SOURCES = src/main_evolution.cpp
 EVO_EXEC = bin/evolution.o
 CORR_SOURCES = src/main_correlation.cpp
 CORR_EXEC = bin/correlation.o
+SUSCEP_SOURCES = src/main_suscept.cpp
+SUSCEP_EXEC = bin/suscept.o
 
 t_crit: $(T_CRIT_EXEC)
 finite: $(FINITE_EXEC)
 evo: $(EVO_EXEC)
 correlation: $(CORR_EXEC)
+suscep: $(SUSCEP_EXEC)
 
 $(T_CRIT_EXEC): $(T_CRIT_SOURCES)
 	g++ -o $(T_CRIT_EXEC) $(T_CRIT_SOURCES) $(LIBS) -O3;
@@ -28,3 +31,7 @@ $(EVO_EXEC): $(EVO_SOURCES)
 $(CORR_EXEC): $(CORR_SOURCES)
 	g++ -o $(CORR_EXEC) $(CORR_SOURCES) $(LIBS) -O3;
 	$(CORR_EXEC)
+
+$(SUSCEP_EXEC): $(SUSCEP_SOURCES)
+	g++ -o $(SUSCEP_EXEC) $(SUSCEP_SOURCES) $(LIBS) -O3;
+	$(SUSCEP_EXEC)
---
+++ src/main_suscept.cpp
@@ -0,0 +1,78 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cmath>
+
+#include "ising.h"
+#include "func.h"
+
+using namespace std;
+
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0])) // length of an array
+
+int L[5] = {20, 40, 60, 80, 100};
+double T_min = 0.5, T_max = 4.0, n_T = 50, T = T_min;
+double n_trans = 20000;
+double n_sweeps = 10000;
+int n_bootstrap = 1000;
+string folder = "result/data/";
+
+
+double Variance(const vector<double>& data) {
+  double mean = 0, var = 0;
+  for (int i = 0; i < data.size(); ++i) {
+    mean += data[i];
+  }
+  mean /= double(data.size());
+  for (int i = 0; i < data.size(); ++i) {
+    var += (data[i] - mean)*(data[i] - mean);
+  }
+  return var/double(data.size());
+}
+
+
+double Susceptibility(const vector<double>& data, void* params) {
+  // cons[0] = T, cons[1] = L
+  double* cons = (double *) params;
+  double var = Variance(data);
+  return cons[1]*cons[1]/cons[0]*var;
+}
+
+
+int main() {
+  double T_step = (T_max - T_min)/(n_T - 1);
+  string filename = folder + "suscep.dat";
+  FILE * datafile = fopen(filename.c_str(), "w");
+
+  for (int l = 0; l < NELEMS(L); ++l) {
+    T = T_min;
+    Ising ising(L[l], T);
+    ising.SetUniformState();
+
+
+    for (int t = 0; t < n_T; ++t) {
+      ising.SetT(T);
+      T += T_step;
+      vector<double> M;
+      for (int i = 0; i < n_trans; ++i) {
+        for (int j = 0; j < ising.L()*ising.L(); ++j) {
+          ising.Flip();
+        }
+      }
+      for (int i = 0; i < n_sweeps; ++i) {
+        for (int j = 0; j < ising.L()*ising.L(); ++j) {
+          ising.Flip();
+        }
+        M.push_back(fabs(ising.M()/(double(ising.L())*double(ising.L()))));
+      }
+      double cons[] = {ising.T(), double(ising.L())};
+      void* params = &cons;
+      double susc = Susceptibility(M, params);
+      double stdev = sqrt(Bootstrap(n_bootstrap, M, Susceptibility,params));
+      fprintf(datafile, "%d %d %.9f %.9f %.9f\n", l, ising.L(), ising.T(), susc,
+                                                stdev);
+      fflush(datafile);
+    }
+  }
+  fclose(datafile);
+}
